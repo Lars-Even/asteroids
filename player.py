42d5572e3e6e46693711c554de457260c7dfc305
Lars-Even <larsevenstubberud@gmail.com>
2025-06-29 21:42:37 +0200

    fixed draw triangle working state

--- player.py
+++ player.py
@@ -2,13 +2,13 @@ import pygame
 from circleshape import CircleShape
 from constants import *
 
-class Player():
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
 
     def draw(self, screen):
-            pygame.draw.polygon(screen, "white", self.triangle(self.rotation))
+            pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     # in the player class
     def triangle(self):
@@ -23,7 +23,7 @@ class Player():
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rorate(-dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
 
