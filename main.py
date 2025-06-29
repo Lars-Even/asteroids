0ff2ce2c1d4f9cc934859cb6228f84f46dfa8e80
Lars-Even <larsevenstubberud@gmail.com>
2025-06-29 20:52:57 +0200

    Fixed pygame.event.get()

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        for event in pygame.get.event():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
